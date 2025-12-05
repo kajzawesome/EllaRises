b8b13a3db877cf81d42ae768898a65de498e3486
michajones16 <michajones16@gmail.com>
2025-12-05 09:42:48 -0700

    updated styling

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const knex = require("knex")({
   client: "pg",
   connection: {
     host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "mariellesutton",
-    password: process.env.DB_PASSWORD || "password",
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "12345",
     database: process.env.DB_NAME || "ellarises",
     port: process.env.DB_PORT || 5432,
     // SSL handling
